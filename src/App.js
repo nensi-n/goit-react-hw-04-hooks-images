5bd6d11e3d9ed2ad57b1bb5d4eaa144eccc4e761
Anna Savchuk <anna060411@gmail.com>
2021-10-23 19:42:35 +0300

    add hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import Container from "./components/Container/Container";
 import Searchbar from "./components/Searchbar/Searchbar";
@@ -11,8 +11,8 @@ import LoaderItem from "./components/Loader/Loader";
 import Modal from "./components/Modal/Modal";
 import ErrorView from "./components/ShowError/ShowError";
 
-class App extends Component {
-  state = {
+function App() {
+  const [state, setState] = useState({
     query: "",
     images: [],
     largeImageURL: "",
@@ -20,111 +20,114 @@ class App extends Component {
     error: null,
     isLoading: false,
     showModal: false,
-  };
+  });
 
-  componentDidUpdate() {
+  useEffect(() => {
     window.scrollTo({
       top: document.documentElement.scrollHeight,
       behavior: "smooth",
     });
-  }
+  }, [state.page]);
 
-  searchImages = async () => {
-    const { query, page } = this.state;
+  const searchImages = async () => {
+    const { query, page } = state;
 
     if (query.trim() === "") {
       return toast.info(" Please enter search query...");
     }
 
-    this.toggleLoader();
+    toggleLoader();
 
     try {
       const request = await apiService(query, page);
-      this.setState(({ images, page }) => ({
-        images: [...images, ...request],
-        page: page + 1,
+      setState((prevState) => ({
+        ...prevState,
+        images: [...prevState.images, ...request],
+        page: prevState.page + 1,
       }));
       if (request.length === 0) {
-        this.setState({ error: `No results were found for ${query}!` });
+        setState((prevState) => ({
+          ...prevState,
+          error: `No results were found for ${query}!`,
+        }));
       }
-      // else {
-      //   window.scrollTo({
-      //     top: document.documentElement.scrollHeight,
-      //     behavior: "smooth",
-      //   });
-      // }
     } catch (error) {
-      this.setState({ error: "Ooops... something went wrong. Try again." });
+      setState((prevState) => ({
+        ...prevState,
+        error: "Ooops... something went wrong. Try again.",
+      }));
     } finally {
-      this.toggleLoader();
+      toggleLoader();
     }
   };
 
-  handleChange = (e) => {
-    this.setState({ query: e.target.value });
+  const handleChange = (e) => {
+    setState((prevState) => ({ ...prevState, query: e.target.value }));
   };
 
-  handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
-    this.setState({ images: [], page: 1, error: null });
-    this.searchImages();
+    setState((prevState) => ({
+      ...prevState,
+      images: [],
+      page: 1,
+      error: null,
+    }));
+    searchImages();
   };
 
-  onLoadMore = () => {
-    this.searchImages();
+  const onLoadMore = () => {
+    searchImages();
   };
 
-  onOpenModal = (e) => {
-    this.setState({ largeImageURL: e.target.dataset.source });
-    this.toggleModal();
+  const onOpenModal = (e) => {
+    setState((prevState) => ({
+      ...prevState,
+      largeImageURL: e.target.dataset.source,
+    }));
+    toggleModal();
   };
 
-  toggleLoader = () => {
-    this.setState(({ isLoading }) => ({
-      isLoading: !isLoading,
+  const toggleLoader = () => {
+    setState((prevState) => ({
+      ...prevState,
+      isLoading: !prevState.isLoading,
     }));
   };
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
+  const toggleModal = () => {
+    setState((prevState) => ({
+      ...prevState,
+      showModal: !prevState.showModal,
     }));
   };
 
-  render() {
-    const { query, images, largeImageURL, isLoading, showModal, error } =
-      this.state;
+  const { query, images, largeImageURL, isLoading, showModal, error } = state;
 
-    return (
-      <Container>
-        <Searchbar
-          onHandleSubmit={this.handleSubmit}
-          onSearchQueryChange={this.handleChange}
-          value={query}
-        />
+  return (
+    <Container>
+      <Searchbar
+        onHandleSubmit={handleSubmit}
+        onSearchQueryChange={handleChange}
+        value={query}
+      />
 
-        {error && <ErrorView texterror={error} />}
+      {error && <ErrorView texterror={error} />}
 
-        {images.length > 0 && (
-          <ImageGallery images={images} onOpenModal={this.onOpenModal} />
-        )}
+      {images.length > 0 && (
+        <ImageGallery images={images} onOpenModal={onOpenModal} />
+      )}
 
-        {isLoading && <LoaderItem />}
+      {isLoading && <LoaderItem />}
 
-        {!isLoading && images.length > 0 && (
-          <Button onLoadMore={this.onLoadMore} />
-        )}
+      {!isLoading && images.length > 0 && <Button onLoadMore={onLoadMore} />}
 
-        {showModal && (
-          <Modal
-            onToggleModal={this.toggleModal}
-            largeImageURL={largeImageURL}
-          />
-        )}
-        <ToastContainer autoClose={3700} />
-      </Container>
-    );
-  }
+      {showModal && (
+        <Modal onToggleModal={toggleModal} largeImageURL={largeImageURL} />
+      )}
+      <ToastContainer autoClose={3700} />
+    </Container>
+  );
 }
 
 export default App;
--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,43 +1,37 @@
-import { Component } from "react";
+import { useEffect } from "react";
 import { createPortal } from "react-dom";
 import PropTypes from "prop-types";
 import "./Modal.css";
 
 const modalRoot = document.querySelector("#modal-root");
 
-class Modal extends Component {
-  componentDidMount() {
-    window.addEventListener("keydown", this.handleKeyDown);
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener("keydown", this.handleKeyDown);
-  }
-
-  handleKeyDown = (e) => {
+function Modal({ onToggleModal, largeImageURL }) {
+  const handleKeyDown = (e) => {
+    console.log("pooiu");
     if (e.code === "Escape") {
-      this.props.onToggleModal();
+      onToggleModal();
     }
   };
 
-  handleBackdropClick = (e) => {
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => window.removeEventListener("keydown", handleKeyDown);
+  });
+
+  const handleBackdropClick = (e) => {
     if (e.currentTarget === e.target) {
-      this.props.onToggleModal();
+      onToggleModal();
     }
   };
 
-  render() {
-    const { largeImageURL } = this.props;
-
-    return createPortal(
-      <div className="overlay" onClick={this.handleBackdropClick}>
-        <div className="modal">
-          <img src={largeImageURL} alt="" />
-        </div>
-      </div>,
-      modalRoot
-    );
-  }
+  return createPortal(
+    <div className="overlay" onClick={handleBackdropClick}>
+      <div className="modal">
+        <img src={largeImageURL} alt="" />
+      </div>
+    </div>,
+    modalRoot
+  );
 }
 
 Modal.propTypes = {
